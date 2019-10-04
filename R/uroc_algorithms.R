03737d39fddaa5009f31c057164a21d98247777e
evwalz <56073009+evwalz@users.noreply.github.com>
2019-10-04 13:38:59 +0200

    Update uroc_algorithms.R

--- R/uroc_algorithms.R
+++ R/uroc_algorithms.R
@@ -1,14 +1,14 @@
 # This function computes the uROC curve as an weighted average over the ROC curves from the associated rocm
 
-Exact = function(response, predictor){
+Exact <- function(response, predictor) {
 
-  n = length(response)
+  n <- length(response)
   thresholds <- unique(response)[-1]
-  N = length(thresholds)+1
+  N <- length(thresholds)+1
 
-  ncontrols <- (which(duplicated(response)==FALSE)-1)[-1]
-  ncases <- (n-ncontrols)
-  weights_all <- (ncases*ncontrols)
+  ncontrols <- (which(duplicated(response) == FALSE) - 1)[-1]
+  ncases <- (n - ncontrols)
+  weights_all <- (ncases * ncontrols)
   weights = sum(weights_all)
 
   pre.order <- order(predictor, decreasing=TRUE)
@@ -16,29 +16,29 @@ Exact = function(response, predictor){
   response <- response[pre.order]
   dups <- rev(duplicated(rev(predictor)))
   
-  response.binary = lapply(thresholds, function(x, response){as.numeric(response>=x)}, response)
-  Hitrate.all = lapply(response.binary, function(x, dups){c(0,(cumsum(x==1)*sum(x==0))[!dups])}, dups)
-  Farate.all = lapply(response.binary, function(x, dups){c(0,(cumsum(x==0)/sum(x==0))[!dups])}, dups)
-  farate.unique = sort(unique(c(unlist(Farate.all),0)))
+  response.binary <- lapply(thresholds, function(x, response) {as.numeric(response >= x)}, response)
+  Hitrate.all <- lapply(response.binary, function(x, dups) {c(0, (cumsum(x == 1) * sum(x == 0))[!dups])}, dups)
+  Farate.all <- lapply(response.binary, function(x, dups) {c(0, (cumsum(x == 0) / sum(x == 0))[!dups])}, dups)
+  farate.unique <- sort(unique(c(unlist(Farate.all), 0)))
 
   #Initialization
-  final.hit.min.new = rep(0,length(farate.unique))
-  final.hit.max.new = rep(0,length(farate.unique))
-  hit.min.new = rep(0,length(farate.unique))
-  hit.max.new = rep(0,length(farate.unique))
+  final.hit.min.new <- rep(0, length(farate.unique))
+  final.hit.max.new <- rep(0, length(farate.unique))
+  hit.min.new <- rep(0, length(farate.unique))
+  hit.max.new <- rep(0, length(farate.unique))
   
-  for(i in 1:(N-1)){
-    hitrate = Hitrate.all[[i]]
-    farate = Farate.all[[i]]
+  for (i in 1:(N-1)) {
+    hitrate <- Hitrate.all[[i]]
+    farate <- Farate.all[[i]]
 
-    far.group.min <- which(as.numeric(duplicated(farate))==0)
+    far.group.min <- which(as.numeric(duplicated(farate)) == 0)
 
     #only select far-values with steps in graph and corresponding min and max value of hitrate
-    far.min = farate[far.group.min]
-    hit.min = hitrate[far.group.min]
+    far.min <- farate[far.group.min]
+    hit.min <- hitrate[far.group.min]
     
-    roc_curve_max = approxfun(x = farate, y = hitrate, method="linear", ties="ordered")
-    hit.max.new = roc_curve_max(farate.unique)
+    roc_curve_max <- approxfun(x = farate, y = hitrate, method = "linear", ties = "ordered")
+    hit.max.new <- roc_curve_max(farate.unique)
 
     hit.min.new <- hit.max.new
     indx.min <- match(far.min, farate.unique)
@@ -49,57 +49,57 @@ Exact = function(response, predictor){
 
   }
 
-  return(list(Farate = sort(c(farate.unique,farate.unique)), Hitrate = sort(c(final.hit.min.new/weights,final.hit.max.new/weights))))
+  return(list(Farate = sort(c(farate.unique, farate.unique)), Hitrate = sort(c(final.hit.min.new, final.hit.max.new) / weights)))
 }
 
 
 # This function computes an approximation of the uROC curve and takes into account ties in the predictor
 
-Approx1 = function(response, predictor,space.size){
+Approx1 = function(response, predictor, space.size) {
 
-  n = length(response)
-  N = length(unique(response))
+  n <- length(response)
+  N <- length(unique(response))
 
-  ncontrols <- (which(duplicated(response)==FALSE)-1)[-1]
+  ncontrols <- (which(duplicated(response) == FALSE) - 1)[-1]
   ncases <- (n-ncontrols)
   weights_all <- (ncases*ncontrols)
-  weights = sum(weights_all)
+  weights <- sum(weights_all)
 
-  pre.order <- order(predictor, decreasing=TRUE)
+  pre.order <- order(predictor, decreasing = TRUE)
   predictor <- predictor[pre.order]
   dups <- rev(duplicated(rev(predictor)))
 
-  if(is.null(space.size)){
-    space.size = 100
+  if (is.null(space.size)) {
+    space.size <- 100
   }
 
-  InterPoint = seq(0,1,1/space.size)
-  Hit.weighted = rep(0,length(InterPoint))
+  InterPoint <- seq(0, 1, (1 / space.size))
+  Hit.weighted <- rep(0, length(InterPoint))
 
-  for(i in 1:(N-1)){
-    controls = ncontrols[i]
-    cases = ncases[i]
-    response.binary = c(rep(0,controls),rep(1,cases))[pre.order]
-    hitrate = c(0,cumsum(response.binary==1)[!dups]*controls)
-    farate = c(0,cumsum(response.binary==0)[!dups]/controls)
+  for (i in 1:(N-1)) {
+    controls <- ncontrols[i]
+    cases <- ncases[i]
+    response.binary <- c(rep(0, controls), rep(1, cases))[pre.order]
+    hitrate <- c(0, cumsum(response.binary == 1)[!dups] * controls)
+    farate <- c(0, cumsum(response.binary == 0)[!dups] / controls)
 
-    roc_curve <- approxfun(farate, hitrate, method = "linear", ties = "ordered")
+    roc_curve <- approxfun(x = farate, y = hitrate, method = "linear", ties = "ordered")
     Hit.weighted <- roc_curve(InterPoint) + Hit.weighted
   }
-  return(list(Farate = c(0,InterPoint), Hitrate = sort(c(0,Hit.weighted/weights))))
+  return(list(Farate = c(0, InterPoint), Hitrate = sort(c(0, Hit.weighted / weights))))
 }
 
 
 # This function computes an approximation of the uROC curve by ignoring ties in the predictor
 
-Approx2 = function(response, predictor, space.size){
+Approx2 <- function(response, predictor, space.size) {
 
   n = length(response)
   N = length(unique(response))
 
-  ncontrols <- (which(duplicated(response)==FALSE)-1)[-1]
-  ncases <- (n-ncontrols)
-  weights_all <- (ncases*ncontrols)
+  ncontrols <- (which(duplicated(response) == FALSE) - 1)[-1]
+  ncases <- (n - ncontrols)
+  weights_all <- (ncases * ncontrols)
   weights = sum(weights_all)
 
   pre.order <- order(predictor, decreasing=TRUE)
@@ -107,20 +107,20 @@ Approx2 = function(response, predictor, space.size){
   dups <- rev(duplicated(rev(predictor)))
 
   if(is.null(space.size)){
-    space.size = 500
+    space.size <- 500
   }
 
-  InterPoint = seq(0,1,1/space.size)
-  Hit.weighted = rep(0,length(InterPoint))
+  InterPoint <- seq(0, 1, (1 / space.size))
+  Hit.weighted <- rep(0, length(InterPoint))
 
   for (i in 1:(N-1)) {
-    controls = ncontrols[i]
-    cases = ncases[i]
-    resBin = c(rep(0,controls),rep(1,cases))[pre.order]
-    hitrate = c(0,cumsum(resBin==1)[!dups]*controls)
-    farate = c(0,cumsum(resBin==0)[!dups]/controls)
+    controls <- ncontrols[i]
+    cases <- ncases[i]
+    resBin <- c(rep(0, controls), rep(1, cases))[pre.order]
+    hitrate <- c(0, cumsum(resBin == 1)[!dups] * controls)
+    farate <- c(0, cumsum(resBin == 0)[!dups] / controls)
 
-    roc_curve = stepfun(x = farate[-1], y = hitrate)  
+    roc_curve <- stepfun(x = farate[-1], y = hitrate)  
     Hit.weighted <- Hit.weighted + roc_curve(InterPoint)
   }
 
